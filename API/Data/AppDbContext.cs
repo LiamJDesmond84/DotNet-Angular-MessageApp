6de6cff65be5be17330c07fd0671afb4b6419cad
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-23 15:31:44 -0600

    Adding DbContext

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class AppDbContext : DbContext
     {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        DbSet<AppUser> Users { get; set; }
     }
 }
