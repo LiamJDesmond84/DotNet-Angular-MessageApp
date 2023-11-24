5b45396cd275e297fae75db2fbcbabf169653dab
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-24 16:35:33 -0600

    Adding misc

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule2 } from './app/app.module';
+import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule2)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
