65af5433f860f0677cdabbb31a07cb274347fdc0
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-24 19:10:56 -0600

    Adding CORS

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
