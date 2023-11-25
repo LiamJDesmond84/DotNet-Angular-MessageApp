c2db49b52ee9bf4a03f7eb07c948f85b91f00e22
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-24 18:39:17 -0600

    Updating main app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title: string = 'Message App';
   users: any;
 
   constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 
 }
