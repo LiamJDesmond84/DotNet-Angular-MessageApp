426b9d0def8d1e28f67f680ae9a5d902a5667d20
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-24 19:14:57 -0600

    Adding CORS

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
