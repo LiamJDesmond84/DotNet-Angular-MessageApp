70d6b23166d3a1c0f54a64bb68f0623adeb5992b
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-25 13:56:32 -0600

    Updating AccountsController

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -2,6 +2,7 @@
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Controllers
 {
@@ -18,7 +19,11 @@ namespace API.Controllers
         public async Task<ActionResult<AppUser>> Register(string userName, string password)
         {
             using var hmac = new HMACSHA512();
-            var user = await _context.Users.Add(appUser);
+            var user = new AppUser{
+                
+                UserName = userName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            }
 
         }
 
