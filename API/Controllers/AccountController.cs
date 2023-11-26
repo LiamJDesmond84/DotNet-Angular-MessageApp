2b94e8061d1438c207bfe26041cffca2143d4cf8
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-26 12:01:17 -0600

    Adding TokenKEy

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,8 +42,9 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return new UserDto
-            { Username = user.UserName,
-              Token = _tokenService.CreateToken(user)};
+            { 
+                 Username = user.UserName,
+                 Token = _tokenService.CreateToken(user)};
             }
 
         [HttpPost("Login")]
