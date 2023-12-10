b8a888cb2166d4f7552d0c0417f62f2291d82614
Liam James Desmond <liamjdesmond84@gmail.com>
2023-12-10 09:57:47 -0600

    Adding user check to authGuard

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -2,11 +2,21 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const accountService = inject(AccountService);
   const toastr = inject(ToastrService);
 
-  return true;
+  return accountService.currentUser$.pipe(
+    map(user => {
+      if(user){
+        return true;
+      }
+      else{
+        toastr.error('You must be logged in');
+      }
+    })
+  );
 };
