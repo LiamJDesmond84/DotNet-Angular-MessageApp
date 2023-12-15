5f05b5875096ade6e2e09e478074a1d0568d67e6
Liam James Desmond <liamjdesmond84@gmail.com>
2023-12-15 16:42:23 -0600

    Updating ExceptionMiddleware class

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -19,8 +19,10 @@
             {
                 await _next(context);
             }
-            catch {
-                (Exception ex) { }
+            catch (Exception ex)
+                    {
+                _logger.LogError(ex, ex.Message);
+                    }
             }
         }
     }
