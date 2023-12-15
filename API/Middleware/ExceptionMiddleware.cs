17e3f4537e54a71c9d95b956e16a8d630285454b
Liam James Desmond <liamjdesmond84@gmail.com>
2023-12-15 16:44:58 -0600

    Updating ExceptionMiddleware class

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Middleware
+﻿using System.Net;
+
+namespace API.Middleware
 {
     public class ExceptionMiddleware
     {
@@ -20,9 +22,12 @@
                 await _next(context);
             }
             catch (Exception ex)
-                    {
+            {
                 _logger.LogError(ex, ex.Message);
-                    }
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment();
             }
         }
     }
