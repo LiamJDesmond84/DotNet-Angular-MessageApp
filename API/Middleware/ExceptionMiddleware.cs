a0b157ba56e3ff4db38243cdc2bd1b1fbeb812b1
Liam James Desmond <liamjdesmond84@gmail.com>
2023-12-15 16:36:30 -0600

    Updating ExceptionMiddleware class

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -2,8 +2,15 @@
 {
     public class ExceptionMiddleware
     {
-        public ExceptionMiddleware(RequestDelegate next, ILogger<Exception> logger)
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
         {
+            _next = next;
+            _logger = logger;
+            _env = env;
         }
     }
 }
