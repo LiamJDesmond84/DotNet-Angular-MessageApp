83d6acc7a8043626d84c400854e9e6ed17b88ff7
Liam James Desmond <liamjdesmond84@gmail.com>
2023-12-15 16:48:41 -0600

    Updating ExceptionMiddleware class

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -34,6 +34,10 @@ namespace API.Middleware
                     : new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
 
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
             }
         }
     }
