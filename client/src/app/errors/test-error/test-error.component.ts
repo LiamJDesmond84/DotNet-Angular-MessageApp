c698bcc1b9f1c205be4ca4f67546d3d323588609
Liam James Desmond <liamjdesmond84@gmail.com>
2023-12-16 21:34:52 -0600

    Adding default stuff

--- client/src/app/errors/test-error/test-error.component.ts
+++ client/src/app/errors/test-error/test-error.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-error',
   templateUrl: './test-error.component.html',
   styleUrls: ['./test-error.component.css']
 })
-export class TestErrorComponent {
+export class TestErrorComponent implements OnInit{
 
+
+	constructor() {}
+  
+  ngOnInit(): void {
+
+  }
   
 }
