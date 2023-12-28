9862bc6ba2f56caeec9155c4a4c1616608b96d7e
Liam James Desmond <liamjdesmond84@gmail.com>
2023-12-27 19:23:18 -0600

    Updating AppUser model

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,6 +9,6 @@
         public DateOnly DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
-        public DateTime LastUpdated { get; set; }
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
     }
 }
